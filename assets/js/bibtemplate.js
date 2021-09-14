6496a54ab0dc38bbad54379bce6aa37b2fc32e8c
Jubayed <ducorteam@gmail.com>
2021-09-14 23:01:10 +0600

    update

--- assets/js/bibtemplate.js
+++ assets/js/bibtemplate.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
 
     $(".btnPub--abstract").on('click', function(){
+
+        bitText = $(this).next()[0];
+        if(bitText){
+           var id =  $(bitText).attr("id").replace("b_", '')
+           $("#" + id).fadeToggle('hide');
+        }
+
         var id = $(this).attr('id').replace("b_", '');
         $("#" + id).fadeToggle(200);
+        
     });
 
     $(".btnPub--BibTex").on('click', function(){
+
+        bitText = $(this).prev()[0];
+        if(bitText){
+           var id =  $(bitText).attr("id").replace("b_", '')
+           $("#" + id).fadeToggle('hide');
+        }
+
         var id = $(this).attr('id').replace("b_", '');
         $("#" + id).fadeToggle(200);
     });
